26dfc98f18dfd7b324c877fb659b0e74819e745a
Maddy Baum <70274731+maddybaum@users.noreply.github.com>
2023-03-12 17:43:24 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -10,10 +10,10 @@
 
 
 // For problems 2-3 use the following lines of code:
-var amysAge = 29;
-var brittanisAge = 34;
-var amysBirthYear = 1991;
-var brittanisBirthYear = 1986;
+let amysAge = 29;
+let brittanisAge = 34;
+let amysBirthYear = 1991;
+let brittanisBirthYear = 1986;
 
 ////////// PROBLEM 2 //////////
 
